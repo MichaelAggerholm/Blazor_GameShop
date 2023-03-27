b4d9bdf2d24399a1d668e3622ed3646a111c5143
Aggerholm <92aggerholm@gmail.com>
2023-03-27 20:26:28 +0200

    tilføjet services for product til blazor client

---
+++ GameShop/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using GameShop.Shared;
+
+namespace GameShop.Client.Services.ProductService
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+
+        Task GetProductsAsync();
+    }
+}
