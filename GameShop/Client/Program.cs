c4d8dd2569d8fed1baacb3334b958eb596901d72
Aggerholm <92aggerholm@gmail.com>
2023-04-01 11:38:30 +0200

    Tilføjet Category service til klient app

--- GameShop/Client/Program.cs
+++ GameShop/Client/Program.cs
@@ -1,6 +1,7 @@
 global using GameShop.Shared;
 global using System.Net.Http.Json;
 global using GameShop.Client.Services.ProductService;
+global using GameShop.Client.Services.CategoryService;
 using GameShop.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -11,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 await builder.Build().RunAsync();
