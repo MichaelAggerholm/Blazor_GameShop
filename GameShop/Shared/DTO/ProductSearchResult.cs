b4f993554ecafb376c295aeb4cd24dbfeefdf21c
Michael Aggerholm <92aggerholm@gmail.com>
2023-04-22 20:31:28 +0200

    Tilføjet productSearchResult dto, med produkter, pages og currentPage

---
+++ GameShop/Shared/DTO/ProductSearchResult.cs
@@ -0,0 +1,10 @@
+namespace GameShop.Shared.DTO;
+
+public class ProductSearchResult
+{
+    public List<Product> Products { get; set; } = new List<Product>();
+    
+    public int Pages { get; set; }
+    
+    public int CurrentPage { get; set; }
+}
